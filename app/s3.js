5a867597ddc35a4de2096bb85c3fab85961b156d
Kathryn Schuler <kathryn.schuler@gmail.com>
2024-11-16 14:22:59 -0500

    update

--- app/s3.js
+++ app/s3.js
@@ -4,10 +4,13 @@ const config = require('./config');
 
 // AWS S3 Configuration
 const s3 = new AWS.S3({
+    forcePathStyle: false,
     region: config.aws.region,
     endpoint: 'https://nyc3.digitaloceanspaces.com', 
-    accessKeyId: config.aws.accessKeyId,
-    secretAccessKey: config.aws.secretAccessKey,
+    credentials: {
+        accessKeyId: config.aws.accessKeyId,
+        secretAccessKey: config.aws.secretAccessKey
+    }
   });
 
 // const upload = multer({
